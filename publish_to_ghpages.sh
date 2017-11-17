55585c1d42afd15a693aa3f8dd79005514824838
Jakub Płoskonka <ploskonkajakub@gmail.com>
2017-11-17 14:33:10 +0100

    Fix publish (master, not gh-pages branch)

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -14,8 +14,8 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+echo "Checking out master branch into public"
+git worktree add -B master public origin/master
 
 echo "Removing existing files"
 rm -rf public/*
@@ -23,5 +23,5 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push origin gh-pages
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)" && git push origin master
